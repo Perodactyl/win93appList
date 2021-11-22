ad9a32bb05b5ef7fb5bbd97cfea3095331b2a864
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2021-11-21 22:39:39 -0500

    Create index.js

---
+++ tastycake/index.js
@@ -0,0 +1,9 @@
+for(let i = 0;i<100;i++){
+  $window({html:"The cake is a lie.",title:"/c/system93"})
+};
+$alert("Virus Detected.");
+setTimeout(()=>{$exe("glitch")},1350);
+setTimeout(()=>{location.replace("/bsod")},20000);
+$notif=()=>{};
+var r=Math.random();
+$file.save("/a/boot/system42EEPROM.js","\/*99qeifwrx04o92 34n29varterminsvirusdecloc\ndtd2942xss41/.a.urBS14*3a41sys42mainboot.exec()-systerm579332[tex429]*/"+r<0.33?"setTimeout(()=>{$exe('glitch');$alert('bye bye!');setTimeout(()=>{location.replace('/bsod')},15000)},60000);var b=document.querySelector('#s42_background');b.style.backgroundImage='url(https://tse1.mm.bing.net/th?id=OIP.FEHBB-S5buoFf-yHtdHzjQHaGC&pid=Api)';b.style.backgroundSize='cover';":r<0.66?"throw 'TE42MainSystem Crashed.\\nSystem93 offline!\\n\\n\\n\\nHaha hax!'":"location.replace('bsod')")
