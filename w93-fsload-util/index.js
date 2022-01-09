06080aaff90a356bc496182a3a731e1e1dabbdc1
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2022-01-09 14:47:36 -0500

    Add files via upload

---
+++ w93-fsload-util/index.js
@@ -0,0 +1,19 @@
+async function restartAppManInstall(fsHandle){
+	sessionStorage.setItem("verboseMode", "no")
+	var installHandle = await fsHandle.getFileHandle("install.js")
+	var installFile = await installHandle.getFile()
+	var text = await installFile.text()
+	var readFsHandle = fsHandle
+	eval(text)
+}
+(async ()=>{
+	var fsHandle
+	document.addEventListener("keydown", (ev)=>{
+		if(ev.key == "r" && ev.altKey){
+			if(ev.shiftKey){
+				fsHandle = await showDirectoryPicker()
+			}
+			restartAppManInstall(fsHandle)
+		}
+	})
+})()
