f5ae05b5f624d9ccdd557335410b2c7c5c72e1b8
Perodactyl <81657800+Perodactyl@users.noreply.github.com>
2022-01-09 16:17:05 -0500

    Added safeguard

--- fsload/index.js
+++ fsload/index.js
@@ -1,12 +1,6 @@
-async function restartAppManInstall(fsHandle){
-	sessionStorage.setItem("verboseMode", "no")
-	var installHandle = await fsHandle.getFileHandle("install.js")
-	var installFile = await installHandle.getFile()
-	var text = await installFile.text()
-	var readFsHandle = fsHandle
-	eval(text)
-}
 (async ()=>{
+	if(window.includes["hasFSLoad"])return
+	window["hasFSLoad"] = true
 	var fsHandle
 	document.addEventListener("keydown", async (ev)=>{
 		if(ev.key == "r" && ev.altKey){
@@ -17,3 +11,11 @@ async function restartAppManInstall(fsHandle){
 		}
 	})
 })()
+async function restartAppManInstall(fsHandle){
+	sessionStorage.setItem("verboseMode", "no")
+	var installHandle = await fsHandle.getFileHandle("install.js")
+	var installFile = await installHandle.getFile()
+	var text = await installFile.text()
+	var readFsHandle = fsHandle
+	eval(text)
+}
